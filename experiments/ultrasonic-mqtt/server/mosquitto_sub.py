aa275ed3a252b6a7cd2d3f2a4d042d28ea3bc361
gpassos01 <gabriel.passos@unesp.br>
2025-02-10 23:26:27 -0300

    Implementação do json no servidor

--- experiments/ultrasonic-mqtt/server/mosquitto_sub.py
+++ experiments/ultrasonic-mqtt/server/mosquitto_sub.py
@@ -1,9 +1,26 @@
 import subprocess
+import json
 
-BROKER_IP = "localhost"  # IP do seu broker Mosquitto (substitua se necessário)
-TOPIC = "sensor/dados"  # Tópico MQTT a ser escutado
+def carregar_configuracoes():
+    """Carrega as configurações de rede a partir de um arquivo JSON."""
+    try:
+        with open('../config.json', 'r') as f:
+            config = json.load(f)  # Carrega o conteúdo JSON
+            return config
+    except FileNotFoundError:
+        print("Arquivo de configurações não encontrado!")
+        return None
+    except json.JSONDecodeError:
+        print("Erro ao ler o arquivo JSON!")
+        return None
 
 def executar_mosquitto_sub():
+
+    config = carregar_configuracoes()
+
+    BROKER_IP = config['mqtt']['broker']  # IP do seu broker Mosquitto (substitua se necessário)
+    TOPIC = config['mqtt']['topic1'] # Tópico MQTT a ser escutado
+
     try:
         # Comando para assinar o tópico no Mosquitto
         comando = ["mosquitto_sub", "-h", BROKER_IP, "-t", TOPIC, "-v"]
